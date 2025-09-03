ba20cf772d6038985a190f6916369a00dc027ee0
ViktorSchmidtTrawinski <vi.trawinski@gmail.com>
2025-09-03 20:42:31 -0300

    implementação da logica da função anagrama

--- solucoes.py
+++ solucoes.py
@@ -1,7 +1,15 @@
 def sao_anagramas(string1, string2):
   #TODO: implementar a lógica
-  pass
+  if sorted(string1.lower())==sorted(string2.lower()):
+    print(True)
+  else:
+    print(False)
+  
+
 
 def cifra_de_cesar(texto, deslocamento):
   #TODO: implementar a lógica
   pass
+
+
+
