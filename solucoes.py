8540117e25a4c00a23ee755e7c206de3f737c17e
.. ... <gabriel.luanh@yahoo.com.br>
2025-09-03 21:14:48 -0300

    alteraçoes realizadas apos analise impressionante

--- solucoes.py
+++ solucoes.py
@@ -3,15 +3,10 @@ def sao_anagramas(string1, string2):
   pass
 
 def encontrar_maior_palavra(texto):
-  frase = texto
-  lista = frase.split()
+  lista = texto.split()
   maior = ""
   for palavra in lista:
       if len(palavra) > len(maior):
           maior = palavra
   print(maior)
-  pass
-
-encontrar_maior_palavra("o rato roeu a ropa do rei de roma")
-
   
