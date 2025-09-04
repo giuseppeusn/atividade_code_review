9c6c0c1b482f0c6a2501d6fae28e5cd50135de96
.. ... <gabriel.luanh@yahoo.com.br>
2025-09-03 21:01:55 -0300

    função de encontrar maior palavra implementada

--- solucoes.py
+++ solucoes.py
@@ -2,6 +2,16 @@ def sao_anagramas(string1, string2):
   #TODO: implementar a lógica
   pass
 
-def cifra_de_cesar(texto, deslocamento):
-  #TODO: implementar a lógica
+def encontrar_maior_palavra(texto):
+  frase = texto
+  lista = frase.split()
+  maior = ""
+  for palavra in lista:
+      if len(palavra) > len(maior):
+          maior = palavra
+  print(maior)
   pass
+
+encontrar_maior_palavra("o rato roeu a ropa do rei de roma")
+
+  
